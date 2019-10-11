43a1b262b435a1ef7e84e681631d30b4a077ce7d
Romanovitch <rokos.job.ru@gmail.com>
2019-10-11 11:40:41 +0300

    feat: implement zeros()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function zeros(expression) {
   // your solution
+  function divideToBase(iNumber, base, counter){
+      return (!(iNumber % base)) 
+          ? divideToBase(iNumber / base, base, ++counter) 
+          : counter;
+  }
+  function browseNumbers(factNumber, onceOrDuble, fiveCounter, twoCounter){
+
+      fiveCounter += divideToBase(factNumber, 5, 0);
+      twoCounter += divideToBase(factNumber, 2, 0);
+      return (factNumber > 1 && (factNumber - onceOrDuble) > 0) 
+          ? browseNumbers(factNumber - onceOrDuble, onceOrDuble, fiveCounter, twoCounter) 
+          : {
+              five: fiveCounter, 
+              two: twoCounter
+          };   
+  }
+  function reducer(sum, iArr){
+           return [
+            sum[0] = (sum[0]) ? sum[0] += iArr.five : iArr.five, 
+            sum[1] = (sum[1]) ? sum[1] += iArr.two : iArr.two
+           ]
+  }
+  function compare(){
+    return 
+  }
+  return expression.split(/[\*]/)
+    .map((el) => el.match(/(\d+)(!+)/))
+    .map(iArr => iArr = browseNumbers(iArr[1], iArr[2].length, 0, 0) )  
+    .reduce(reducer, 0)
+    .reduce((res, iElem) => (res === false || res > iElem) ? iElem : res, false)
 }
